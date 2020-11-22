df3c5024daf70145b8eee69cee9b6eb20513c9cc
Michael Bullington <mikebullingtn@gmail.com>
2020-11-22 02:50:35 +0000

    see if this is a GH actions caching issue

--- build.sh
+++ build.sh
@@ -85,7 +85,7 @@ build () {
   YYYYMMDD="$(date +%Y%m%d)"
   OUTPUT_DIR="$BASE_DIR/builds/$BUILD_ARCH"
   mkdir -p "$OUTPUT_DIR"
-  FNAME="elementaryos-$VERSION-$CHANNEL.$YYYYMMDD$OUTPUT_SUFFIX"
+  FNAME="live-image"
   mv "$BASE_DIR/tmp/$BUILD_ARCH/live-image-$BUILD_ARCH.hybrid.iso" "$OUTPUT_DIR/${FNAME}.iso"
 
   # cd into output to so {FNAME}.sha256.txt only
