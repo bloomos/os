f2d3caccc0667ad2b851777c964a4d9aeaf0ba1a
Michael Bullington <michael@bullington.xyz>
2021-03-14 18:53:07 -0400

    use hostnamectl instead of homebrew solution

--- includes.chroot/etc/runonce.d/10-add-hostname.sh
+++ includes.chroot/etc/runonce.d/10-add-hostname.sh
@@ -51,7 +51,6 @@ NOUNS=(
     'rabbit'
     'snail'
     'shrimp'
-    'tang'
     'umpire'
     'valet'
     'wasp'
@@ -72,16 +71,4 @@ generate_hostname()
 }
 
 HOSTNAME=$(generate_hostname)
-
-echo $HOSTNAME
-
-echo $HOSTNAME > /etc/hostname
-
-cat << EOF > /etc/hosts
-127.0.0.1       $HOSTNAME    localhost
-::1             localhost ip6-localhost ip6-loopback
-fe00::0         ip6-localnet
-ff00::0         ip6-mcastprefix
-ff02::1         ip6-allnodes
-ff02::2         ip6-allrouters
-EOF
+hostnamectl set-hostname $HOSTNAME
